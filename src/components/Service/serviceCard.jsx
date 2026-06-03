15eb81e3fc5a3e624a178903cfc07bd534ac15f3
omarhatemsobhymostafa <omar.hatem.okasha@gmail.com>
2026-06-03 21:59:56 +0300

    fixed dark

--- src/components/Service/serviceCard.jsx
+++ src/components/Service/serviceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function serviceCard(props) {
     return (
-           <div class="max-w-sm p-6 bg-gray-50 dark:bg-gray-800 rounded-lg shadow content-center">
+           <div class="max-w-sm p-6 bg-gray-800 rounded-lg shadow content-center">
 
                     <img
                         src={props.img}
@@ -10,7 +10,7 @@ export default function serviceCard(props) {
                         class="w-1/2 h-1/2 mb-7 mx-auto"
                     />
 
-                    <h3 class="text-xl font-bold mb-2 text-gray-900 dark:text-white">
+                    <h3 class="text-xl font-bold mb-2 text-white">
                         {props.title}
 
                     </h3>
--- src/components/projects/projectCard.jsx
+++ src/components/projects/projectCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function projectCard(props) {
     return (
-        <div class="max-w-sm p-6 bg-gray-50 dark:bg-gray-800 rounded-lg shadow content-center">
+        <div class="max-w-sm p-6 bg-gray-800 dark:bg-gray-800 rounded-lg shadow content-center">
 
             <div class="flex justify-center rounded-full border-2 border-gray-300 content-center mx-auto h-24 w-24 mb-4">
                 <img class="w-24 h-24  rounded-full scale-75" src={props.img} alt="Project Image" />
             </div>
 
-            <h3 class="text-xl font-bold mb-2 text-gray-900 dark:text-white">
+            <h3 class="text-xl font-bold mb-2 text-white">
                 {props.title}
 
             </h3>
